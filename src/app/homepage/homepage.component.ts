c5eb73902d480ccfe1fbcc91395588af929fe57c
saamirkc <samir.kc.a20@gmail.com>
2025-01-03 21:04:07 +0545

    commiting after making change

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import {Event, Router} from '@angular/router';
 import {ConnectService} from '../connect.service';
 import {HttpClient} from '@angular/common/http';
 import {response} from 'express';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-homepage',
@@ -61,16 +62,32 @@ console.log(this.selectedPicture)
       this.imageUploadService.uploadImage(this.selectedPicture).subscribe(
         (response: any) => {
           console.log('Server Response:', response); // Log the response to check its structure
+          Swal.fire({
+            title: "Image Uploaded Successfully !",
+            icon: "success",
+            background:"rgba(104,51,153,0.5)",
+            color:"rgb(255,255,255)",
+            customClass:{
+              popup:'swal-custom-popup'
+            },
+            showConfirmButton:false,
+            timer: 1500
 
+
+
+
+
+          });
           if (response.localized_image) {
             // Handle Base64 localized image
             this.uploadedImage = `data:image/png;base64,${response.localized_image}`;
-            this.message = `Prediction: ${response.diagnosis}`;
+            this.message = `Prediction: ${response.diagnosis}`+'!' +'The affected part of the lungs due to pneumonia is shown by the  bounding box in the above localized image.';
             this.level ='error'
           } else {
-            this.message = 'Prediction : '+ response.diagnosis +'.'+'No localized image returned from the backend.';
+            this.message = 'Prediction : '+ response.diagnosis +'!'+' No localized image returned from the backend.';
             console.error('Localized image missing in response:', response);
             this.level='success'
+
           }
         },
         (error) => {
